673153d23db5b0a0488abba67e10126c7d17040d
Brian <briankhalawon@gmail.com>
2025-04-06 13:32:32 +0100

    Deleted unnecessary file and added night watch

---
+++ custom-tests/front-page.test.js
@@ -0,0 +1,7 @@
+describe('Basic pages', function() {
+    it('check the front page loads', function(browser) {
+      browser
+        .navigateTo('http://localhost:3000')   // Navigates to the front page
+        .waitForElementVisible('body');       // Waits for the <body> element to be visible
+    });
+});
