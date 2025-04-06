a68af20e3cefa642fc78413c28c3aec35f1778df
Brian <briankhalawon@gmail.com>
2025-04-06 18:45:06 +0100

    added db test for nightwatch

--- custom-tests/front-page.test.js
+++ custom-tests/front-page.test.js
@@ -5,3 +5,12 @@ describe('Basic pages', function() {
         .waitForElementVisible('body');       // Waits for the <body> element to be visible
     });
 });
+
+describe('Basic pages', function() {
+  it('check the database page', function(browser) {
+    browser
+      .useXpath()
+      .navigateTo('http://localhost:3000/db_test')
+      .assert.textContains('/html/body/pre', 'xxx');
+  }); 
+});
