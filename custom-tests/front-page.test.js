47bb1ed9f0dceaae2faaaac674f2f67f5ec0cb17
Brian <briankhalawon@gmail.com>
2025-04-06 18:55:06 +0100

    Reverted changes to package and frontpagetest

--- custom-tests/front-page.test.js
+++ custom-tests/front-page.test.js
@@ -6,11 +6,3 @@ describe('Basic pages', function() {
     });
 });
 
-describe('Basic pages', function() {
-  it('check the database page', function(browser) {
-    browser
-      .useXpath()
-      .navigateTo('http://localhost:3000/db_test')
-      .assert.textContains('/html/body/pre', 'xxx');
-  }); 
-});
